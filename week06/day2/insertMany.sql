e861e2cff17f378e5b003a166f7985461cde66ec
hardikmaghrola <hardymango3@gmail.com>
2022-03-31 14:45:32 +1100

    my first commit

---
+++ week06/day2/insertMany.sql
@@ -0,0 +1,8 @@
+-- insert 5 rows into the loyalty table
+insert into loyalty.customers (familyName,givenName,loyaltyNum,contactNumber,email,points)
+values
+("Pauls","Alice",109899,"89776123","Alice.P@gmail.com",1144),
+("Lu","Lucy",239000,"02-99977612","lucylue@gmail.com",1314),
+("Jones","Alex",111899,"03-95577613","jonesalex@gmail.com",2300),
+("Laker","Tim",202899,"06-55776123","laker.t@gmail.com",1444),
+("Muller","Thomas",239099,"98776123","muller.t@gmail.com",2004);
